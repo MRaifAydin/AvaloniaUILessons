7b6399b885d64c14c996ee5ea69055e6d337de56
MRaifAydin <mert5708@gmail.com>
2023-05-20 08:35:05 +0300

    First Lesson

---
+++ AvaloniaUILesson1/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUILesson1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
